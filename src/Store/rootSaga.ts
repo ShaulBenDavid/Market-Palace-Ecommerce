71d9ead9611514d58c54ec69d0b7c17776ecc050
Shaul Ben david <Shaulbd9@gmail.com>
2022-08-11 19:43:30 +0300

    ts

--- src/Store/rootSaga.ts
+++ src/Store/rootSaga.ts
@@ -1,8 +1,8 @@
-import { all, call } from "redux-saga/effects";
+import { all, call } from "typed-redux-saga/macro";
 
 import { categoriesSaga } from "./Categories/Categories.saga";
 import { userSaga } from "./User/User.saga";
 
 export function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSaga)]);
+  yield* all([call(categoriesSaga), call(userSaga)]);
 }
