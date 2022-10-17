0b4c56a2537acc6f280f4f2b9782ce91f08288dc
Shaul Ben david <Shaulbd9@gmail.com>
2022-10-17 18:49:44 +0300

    pwa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./Store/Store";
 import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorkers from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -25,3 +26,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkers.register();
